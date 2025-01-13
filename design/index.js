3e14ee2a20b412e635de6b7e0f75ff51adedf0ac
Svetlana Bogomolova <84743017+svetlanabog188419@users.noreply.github.com>
2025-01-12 21:29:49 -0500

    Added serch functionality using the API

--- design/index.js
+++ design/index.js
@@ -1,11 +1,28 @@
+function refreshWeather(response){
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    temperatureElement.innerHTML= Math.round(temperature);
+    city.innerHTML=response.data.city;
+
+}
+
+
+function searchCity(city){
+    let apiKey = "7cb48858f3d457ao4f0b447cccc30t2f";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    //console.log(apiUrl);
+   axios.get(apiUrl).then(refreshWeather);
+
+
+}
+
 
 
 function handleSearchSubmit(event){
 event.preventDefault();
 let searchInput = document.querySelector("#search-form-input");
 let city = document.querySelector("#city");
-
-city.innerHTML=searchInput.value;
+searchCity(searchInput.value);
 
 }
 
@@ -13,3 +30,5 @@ city.innerHTML=searchInput.value;
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit); 
+
+searchCity("New York");
