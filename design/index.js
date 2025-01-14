8cfadfd62a13c3e05f39989c77e095ac65384ee5
Svetlana Bogomolova <84743017+svetlanabog188419@users.noreply.github.com>
2025-01-13 22:21:04 -0500

    inject more weather data

--- design/index.js
+++ design/index.js
@@ -1,10 +1,34 @@
 function refreshWeather(response){
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
+    let descriptionElement=document.querySelector("#description");
+    let humidityElement=document.querySelector("#humidity");
+    let windElement=document.querySelector("#wind");
+    let timeElement=document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    
     temperatureElement.innerHTML= Math.round(temperature);
+    descriptionElement.innerHTML=response.data.condition.description;
+    humidityElement.innerHTML=`${response.data.temperature.humidity}%`;
+    windElement.innerHTML=`${response.data.wind.speed}km/h`;
+    timeElement.innerHTML= formatDate(date);
     city.innerHTML=response.data.city;
-
+     console.log(response.data);
 }
+
+function formatDate(date){
+    
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
+    if(minutes < 10) {
+        minutes = `0${minutes}`;
+    }
+
+    return `${day} ${hours}:${minutes},` 
+
+    }
 
 
 function searchCity(city){
