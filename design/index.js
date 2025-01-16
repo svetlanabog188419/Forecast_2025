fded07dd094951705ac36d4105de768a620b2a9b
Svetlana Bogomolova <84743017+svetlanabog188419@users.noreply.github.com>
2025-01-16 09:28:06 -0500

    Injected the forecast HTML from JS

--- design/index.js
+++ design/index.js
@@ -53,9 +53,34 @@ searchCity(searchInput.value);
 
 }
 
+function displayForecast () {
+
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+
+    days.forEach(function (day) {
+
+    
+forecastHtml = forecastHtml + `<div class="weather-forecast-day">
+<div class="weather-forecast-date">${day}</div>
+ <div class="weather-forecast-icon">☀️</div>
+  <div class="weather-forecast-temperatures"> 
+   <div class="weather-forecast-temperature"> 
+    <strong>15º</strong>
+   </div>
+     <div class="weather-forecast-temperature">9º</div>
+  </div>
+</div>`;
+});
+
+let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = forecastHtml;
+
+}
 
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit); 
-
+displayForecast();
 searchCity("New York");
+
